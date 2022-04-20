36c237dfee66fd68771ebeb200f84f95eb3ea528
Fabian Breitling <83815772+msfb3@users.noreply.github.com>
2022-04-20 11:43:31 +0200

    Added basic Controller logic.

--- src/de/breitling/Controller.java
+++ src/de/breitling/Controller.java
@@ -1,4 +1,15 @@
 package de.breitling;
 
 public class Controller {
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+        initiActions();
+    }
+    public void initiActions() {
+
+    }
 }
