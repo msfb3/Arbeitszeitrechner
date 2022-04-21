f4f5a6ab9c89172d0c4d31d0010f251c1d007246
Fabian Breitling <83815772+msfb3@users.noreply.github.com>
2022-04-21 14:51:46 +0200

    Setup Main Class.

--- src/de/breitling/Main.java
+++ src/de/breitling/Main.java
@@ -3,5 +3,8 @@ package de.breitling;
 public class Main {
     public static void main(String[] args) {
 
+        View view = new View();
+        Model model = new Model();
+        Controller controller = new Controller(model,view);
     }
 }
