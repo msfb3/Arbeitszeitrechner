5b4676adce710dd1bc6fd3b0e5a7e3542baff8af
Fabian Breitling <83815772+msfb3@users.noreply.github.com>
2022-04-22 10:59:14 +0200

    Setup View Class.

--- src/de/breitling/View.java
+++ src/de/breitling/View.java
@@ -1,4 +1,55 @@
 package de.breitling;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class View {
+    private JTextField txtBegin;
+    private JTextField txtEnd;
+    private JButton btnUpdate;
+    private JButton btnCalculate;
+    private JFrame window;
+    private JTable timeTable;
+    private JTextField txtUserId;
+
+    public View() {
+        window = new JFrame("Worktime Calc");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JLabel lblBegin = new JLabel("Startzeit: hh:mm");
+        JLabel lblEnd = new JLabel("Endzeit: hh:mm");
+        JLabel lblUserId = new JLabel("MitarbeiterID");
+
+        JPanel timePanel = new JPanel();
+        JPanel timePanelLeft = new JPanel(new BorderLayout(1,1));
+        JPanel timePanelRight = new JPanel(new GridLayout(10,1));
+        JSplitPane timePanelSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+
+        txtBegin = new JTextField();
+        btnUpdate = new JButton("Aktualisieren");
+        btnCalculate = new JButton("Pushen");
+        txtEnd = new JTextField();
+        txtUserId = new JTextField();
+        timeTable = new JTable();
+
+        timePanelLeft.add(new JScrollPane(timeTable));
+        timePanelSplitPane.setLeftComponent(timePanelLeft);
+        timePanelSplitPane.setRightComponent(timePanelRight);
+        timePanel.add(timePanelSplitPane);
+
+
+        timePanelRight.add(lblUserId);
+        timePanelRight.add(txtUserId);
+        timePanelRight.add(lblBegin);
+        timePanelRight.add(txtBegin);
+        timePanelRight.add(lblEnd);
+        timePanelRight.add(txtEnd);
+        timePanelRight.add(btnUpdate);
+        timePanelRight.add(btnCalculate);
+
+
+        window.setSize(600,600);
+        window.add(timePanel);
+        window.setVisible(true);
+
+    }
 }
