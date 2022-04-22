d96e65d3d6658efda30e1479018d4c06eb12d3ba
Fabian Breitling <83815772+msfb3@users.noreply.github.com>
2022-04-22 11:50:45 +0200

    Setup TimeModel Class.

--- src/de/breitling/TimeModel.java
+++ src/de/breitling/TimeModel.java
@@ -1,4 +1,75 @@
 package de.breitling;
 
+import java.text.ParseException;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class TimeModel {
+    private int userID;
+    private LocalTime timeStart;
+    private LocalTime timeEnd;
+    private double timeWorkedMinutes;
+
+    public void calcMinutesWorked(String timeBegin, String timeEnd,int userID) throws ParseException {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        LocalTime timeStart = LocalTime.parse(timeBegin,formatter);
+        LocalTime timeDone = LocalTime.parse(timeEnd,formatter);
+        double minutesWorked = Duration.between(timeStart,timeDone).toMinutes();
+
+        if (minutesWorked >= 600) {
+            minutesWorked = minutesWorked - 45;
+        } else if (minutesWorked >= 360) {
+           minutesWorked = minutesWorked - 30;
+        }
+        this.timeStart = timeStart.truncatedTo(ChronoUnit.MINUTES);
+        this.timeEnd = timeDone.truncatedTo(ChronoUnit.MINUTES);
+        this.timeWorkedMinutes = minutesWorked;
+        this.userID = userID;
+    }
+
+    public TimeModel(LocalTime timeStart, LocalTime timeEnd, int userID) throws ParseException {
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
+        this.userID = userID;
+        calcMinutesWorked(timeStart.toString(),timeEnd.toString(),userID);
+    }
+
+    public TimeModel() {
+
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public LocalTime getTimeStart() {
+        return timeStart;
+    }
+
+    public void setTimeStart(LocalTime timeStart) {
+        this.timeStart = timeStart;
+    }
+
+    public LocalTime getTimeEnd() {
+        return timeEnd;
+    }
+
+    public void setTimeEnd(LocalTime timeEnd) {
+        this.timeEnd = timeEnd;
+    }
+
+    public double getTimeWorkedMinutes() {
+        return timeWorkedMinutes;
+    }
+
+    public void setTimeWorkedMinutes(double timeWorkedMinutes) {
+        this.timeWorkedMinutes = timeWorkedMinutes;
+    }
 }
